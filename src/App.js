dcad9b8fa65dc4295402f9c296456ed0ef3ecda9
Masashi Morita <masashi.morita@opentone.co.jp>
2020-07-31 12:41:32 +0900

    折れ線グラフの描画

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import LineChart from './components/LineChart'
+
 const data =[
     [{x: 0, y: 6},{x: 1, y: 9},{x: 2, y: 6},
         {x: 3, y: 5},{x: 4, y: 2},{x: 6, y: 4},
@@ -15,6 +17,7 @@ class App extends Component {
         return (
             <div>
                 <h2>React D3.js line chart</h2>
+                <LineChart  data={data} />
             </div>
         );
     }
---
+++ src/components/LineChart.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import * as d3 from 'd3';
+
+class LineChart extends Component {
+    width = 500
+    height = 500
+    margin = 50
+
+    constructor(props){
+        super(props);
+        this.width = props.width || this.width;
+        this.height = props.height || this.height;
+        this.margin = props.margin || this.margin;
+        this.createLineChart = this.createLineChart.bind(this);
+    }
+
+    componentDidMount() {
+        this.createLineChart()
+    }
+    componentDidUpdate() {
+        this.createLineChart()
+    }
+
+    createLineChart() {
+        const _self = this;
+        const node = this.node
+        const x = d3.scaleLinear()
+                .domain([0, 10])
+                .range([this.margin, this.width - this.margin]);
+        const y = d3.scaleLinear()
+                .domain([0, 10])
+                .range([this.height - this.margin, this.margin]);
+
+        d3.range(10).map((i) => {
+            return {x: i, y: Math.sin(i) + 5};
+        });
+
+        const line = d3.line()
+            .x((d) => {return x(d.x);})
+            .y((d) => {return y(d.y);});
+
+        const svg = d3.select(node);
+
+        svg.attr("height", this.height)
+            .attr("width", this.width);
+
+        svg.selectAll("path")
+            .data(_self.props.data)
+            .enter()
+            .append("path")
+            .attr("class", "line")
+            .attr("d", (d) => {return line(d);});
+
+        this.renderAxes(svg, x, y);
+    }
+
+    render() {
+        return <svg ref={node => this.node = node}>
+        </svg>
+    }
+
+    renderAxes(svg, x, y){
+        const _self = this
+        const xAxis = d3.axisBottom()
+            .scale(x.range([0, this.quadrantWidth()]))
+            .scale(x);
+
+        const yAxis = d3.axisLeft()
+            .scale(y.range([this.quadrantHeight(), 0]))
+            .scale(y);
+
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", function(){
+                return `translate(${_self.xStart()}, ${_self.yStart()})`
+            })
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", function(){
+                return `translate(${_self.xStart()}, ${_self.yEnd()})`
+            })
+            .call(yAxis);
+    }
+
+    xStart(){ return this.margin;}
+    yStart(){ return this.height - this.margin;}
+    xEnd(){ return this.width - this.margin;}
+    yEnd(){ return this.margin;}
+    quadrantWidth(){ return this.width - 2 * this.margin;}
+    quadrantHeight(){ return this.height - 2 * this.margin;}
+}
+
+export default LineChart
