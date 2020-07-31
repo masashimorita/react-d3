6ffac148a3860c3fa11a0923412dc2e4ac8a8f18
Masashi Morita <masashi.morita@opentone.co.jp>
2020-07-31 11:02:29 +0900

    不要なファイルを削除

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+const data =[
+    [{x: 0, y: 6},{x: 1, y: 9},{x: 2, y: 6},
+        {x: 3, y: 5},{x: 4, y: 2},{x: 6, y: 4},
+        {x: 7, y: 2},{x: 8, y: 5},{x: 9, y: 2}]
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>React D3.js line chart</h2>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
